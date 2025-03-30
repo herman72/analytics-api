5c14521fa1be2e71f237855fa1329376bb41000a
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-30 16:20:04 +0300

    :hammer: event list check

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import EventSchema, EventListSchema
 
 router = APIRouter()
 
 @router.get("/")
-def read_events():
-    return {"results": [1, 2, 3]}
+def read_events() -> EventListSchema:
+    return {
+        "results": [
+            {"id":1}, {"id":2}, {"id":3}
+                    ],
+        "count": 3
+        }
 
 
 @router.get("/{event_id}")
