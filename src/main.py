f92a10c3e069c8441ac2ffdf2cf66840fefd40fa
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-30 16:06:30 +0300

    :hammer: add router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
+from api.events import router as events_router
 
 app = FastAPI()
+app.include_router(events_router, prefix="/api/events")
 
 
 @app.get("/")
