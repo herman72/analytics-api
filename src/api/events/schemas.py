b44f4a3b9bc0c650711d593e66ccd6057781483c
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-30 20:33:59 +0300

    :hammer: schema for put and post

--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,10 +1,24 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, Field
 
+"""
+id
+path
+description    
+"""
+
+class EventCreateSchema(BaseModel):
+    page: str
+    description: Optional[str] = Field(default="")
+
+class EventUpdateSchema(BaseModel):
+    description: str
+    
 
 class EventSchema(BaseModel):
     id: int
-    
+    page: Optional[str] = ""
+    description: Optional[str] = ""
     
 
 class EventListSchema(BaseModel):
