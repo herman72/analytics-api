38fbd016dbb8b04bf079e49747cfd952b8a1b4a7
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-03-30 20:33:39 +0300

    :hammer: sample for post and put

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
-from .schemas import EventSchema, EventListSchema
+from .schemas import (EventSchema, 
+                      EventListSchema, 
+                      EventCreateSchema, 
+                      EventUpdateSchema
+                      )
 
 router = APIRouter()
 
+# List view /api/events
 @router.get("/")
 def read_events() -> EventListSchema:
     return {
@@ -16,3 +21,25 @@ def read_events() -> EventListSchema:
 @router.get("/{event_id}")
 def get_event(event_id: int) -> EventSchema:
     return {"id": event_id}
+
+# Send Data here
+# post view
+# Post /api/events
+@router.post("/")
+def create_events(payload: EventCreateSchema) -> EventSchema:
+    print(payload)
+    data = payload.model_dump()
+    return {"id": 123, **data}
+
+# update data
+# PUT /api/events/12
+@router.put("/{event_id}")
+def update_event(event_id: int, payload: EventUpdateSchema) -> EventSchema:
+    print(payload)
+    data = payload.model_dump()
+    return {"id": event_id, **data}
+
+#Delete
+# @router.delete("/{event_id}")
+# def get_event(event_id: int) -> EventSchema:
+#     return {"id": event_id}
