de4fa97778f1247bbf48fe986d84fb683af10ee5
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-03 09:37:59 +0300

    :hammer: add timesacle connection

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -1,17 +1,20 @@
 import sqlmodel
 from sqlmodel import SQLModel, Session
-from .config import DATABASE_URL
-
+from .config import DATABASE_URL, DB_TIMEZONE
+import timescaledb 
 
 if DATABASE_URL =="":
     raise NotImplementedError("DATABASE URL needs to be set")
-engine = sqlmodel.create_engine(DATABASE_URL)
+# engine = sqlmodel.create_engine(DATABASE_URL)
+engine = timescaledb.create_engine(DATABASE_URL, timezone=DB_TIMEZONE)
 
 
 def init_db():
     print("creating database")
     SQLModel.metadata.create_all(engine)
-    return
+    print("creating hyper tables")
+    timescaledb.metadata.create_all(engine)
+    
 
 def get_session():
     with Session(engine) as session:
