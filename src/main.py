f9f89a76cb20b88d17976fb9db5492a7f27e88df
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-04-08 16:08:56 +0300

    :hammer: added middleware layer

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.events import router as events_router
 from api.db.session import init_db
 from contextlib import asynccontextmanager
@@ -11,6 +12,11 @@ async def lifespan(app: FastAPI):
     return
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials = True,
+                   allow_methods = ["*"],
+                   allow_headers=["*"])
 app.include_router(events_router, prefix="/api/events")
 
 
